debfb950c719f438d6716839c7592bf93c06d0bb
Guilherme-TimeTech <guilherme.alves@chameotto.com.br>
2025-10-18 14:52:22 -0300

    testes de customers

--- spec/features/customer_spec.rb
+++ spec/features/customer_spec.rb
@@ -39,4 +39,100 @@ RSpec.feature "Customers", type: :feature do
     click_on('Criar Cliente')
     expect(page).to have_content('não pode ficar em branco')
   end
+
+  scenario 'Mostra um cliente' do
+    customer = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: [ 'S', 'N' ].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    visit(customer_path(customer.id))
+    expect(page).to have_content(customer.name)
+    expect(page).to have_content(customer.email)
+    expect(page).to have_content(customer.phone)
+  end
+
+  scenario 'Testando a Index' do
+    customer1 = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: [ 'S', 'N' ].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    customer2 = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: [ 'S', 'N' ].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    visit(customers_path)
+    expect(page).to have_content(customer1.name).and have_content(customer2.name)
+  end
+
+  scenario 'Atualiza um Cliente' do
+    customer = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: [ 'S', 'N' ].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    new_name = Faker::Name.name
+    visit(edit_customer_path(customer.id))
+    fill_in('Nome', with: new_name)
+    click_on('Atualizar Cliente')
+
+    expect(page).to have_content('Cliente atualizado com sucesso!')
+    expect(page).to have_content(new_name)
+  end
+
+  scenario 'Clica no link Mostrar da index' do
+    customer = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: [ 'S', 'N' ].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    visit(customers_path)
+    find(:xpath, "/html/body/table/tbody/tr[1]/td[2]/a").click
+    expect(page).to have_content("Mostrando Cliente")
+  end
+
+  scenario 'Clica no link Editar da index' do
+    customer = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: [ 'S', 'N' ].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    visit(customers_path)
+    find(:xpath, "/html/body/table/tbody/tr[1]/td[3]/a").click
+    expect(page).to have_content("Editando Cliente")
+  end
+
+  scenario 'Apaga um cliente', js: true do
+    customer = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: %w[S N].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    visit(customers_path)
+    click_on 'Excluir'
+    expect(page).to have_content('Cliente excluído com sucesso!')
+  end
 end
