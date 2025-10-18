1d24dc59e4188d02eb6a72cbcd4f241c2a287d10
Guilherme-TimeTech <guilherme.alves@chameotto.com.br>
2025-10-18 14:53:10 -0300

    configurando o Turbo JS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
+import Rails from "@rails/ujs"
+Rails.start()
+
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
