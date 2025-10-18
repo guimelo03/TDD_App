2e3d75c20494ba445e3e9965cb8a64efee4fa406
Guilherme-TimeTech <guilherme.alves@chameotto.com.br>
2025-10-18 14:52:45 -0300

    views e controllers CRUD para testes de customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,9 +1,22 @@
 class CustomersController < ApplicationController
+    before_action :set_customer, only: [ :edit, :update, :show, :destroy ]
     def index
+        @customers = Customer.all
     end
 
     def new
         @customer = Customer.new
+    end
+
+    def edit
+    end
+
+    def update
+        if @customer.update(customers_params)
+          redirect_to customer_path(@customer.id), notice: "Cliente atualizado com sucesso!"
+        else
+            render :edit
+        end
     end
 
     def create
@@ -15,6 +28,23 @@ class CustomersController < ApplicationController
         else
             render :new
         end
+    end
+
+    def show
+    end
+
+    def destroy
+        if @customer.destroy
+          redirect_to customers_path, notice: "Cliente excluído com sucesso!"
+        else
+            render :index
+        end
+    end
+
+    private
+
+    def set_customer
+        @customer = Customer.find(params[:id])
     end
 
     def customers_params
